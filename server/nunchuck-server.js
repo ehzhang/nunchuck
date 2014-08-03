139158826756e9eca7b6cd4717449bf4ad76042d
Edwin Zhang <ehzhang@mit.edu>
2014-08-02 19:16:50 -0700

    add error handling for rooms that don't exist

--- server/nunchuck-server.js
+++ server/nunchuck-server.js
@@ -1,29 +1,47 @@
-// Socket acts as a key-value store for routing
+var rooms = {};
+
 module.exports = function(io) {
 
   // Socket code
   io.on('connection', function(socket){
     console.log('A user connected');
 
+    var type, _id;
+
+    socket.on('disconnect', function(){
+      if (type === 'host' && _id){
+        delete rooms[_id]
+      }
+    });
+
     socket.on('nunchuck-create', function(id){
-      console.log("create", id);
       socket.join(id);
+      console.log("Created a room with ID: " + id);
+      rooms[id] = true;
+      type = 'host';
+      _id = id;
+      console.log(rooms);
     });
 
     socket.on('nunchuck-join', function(msg){
+      if (type !== 'host' && rooms[msg.id]){
+        socket.join(msg.id);
+        console.log("User " + msg.username + " joined room " + msg.id);
 
-      socket.join(msg.id);
-      console.log('join', msg.id);
-
-      io.to(msg.id).emit('nunchuck-join',
-        {
-          username: msg.username,
-          id: msg.id,
-          success: true
-        });
-
+        io.to(msg.id).emit('nunchuck-join',
+          {
+            username: msg.username,
+            id: msg.id,
+            success: true
+          });
+      } else {
+        socket.emit('nunchuck-join',
+          {
+            username: msg.username,
+            id: msg.id,
+            success: false
+          });
+      }
     });
-
   });
-
 };
